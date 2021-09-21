07614713ec85a76d1d86b34103b43b1db314090c
gan11a <ivan.tulskuy@yahoo.com>
2021-09-21 16:52:38 +0300

    Added Listener

--- src/test/java/cf/iqas/allure/Selenidetest.java
+++ src/test/java/cf/iqas/allure/Selenidetest.java
@@ -1,8 +1,9 @@
 package cf.iqas.allure;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -13,13 +14,13 @@ import static org.openqa.selenium.By.partialLinkText;
 public class Selenidetest {
     @Test
     public void testGit() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com");
         $(".header-search-input").click();
         $(".header-search-input").setValue("eroshenkoam/allure-example");
         $(".header-search-input").submit();
         $(linkText("eroshenkoam/allure-example")).click();
         $(partialLinkText("Issues")).click();
-        $(byText("#74")).should(Condition.visible);
         $(byText("#74")).should(Condition.visible);
     }
 }
